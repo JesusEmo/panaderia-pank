df2334189a8f218feea6e862e7e72a0cb8419517
Gama <gama.canales@ithk.com.ar>
2022-04-11 11:23:28 -0300

    panaderia pank

---
+++ js.js
@@ -0,0 +1,13 @@
+var chotaxd = "buenas que tal soy una alerta re molesta jaja";
+
+alert(`${chotaxd}`);
+
+function producto(productos){
+    document.getElementById(producto)
+    if(producto=){
+
+    }
+    else if {
+
+    }
+}
