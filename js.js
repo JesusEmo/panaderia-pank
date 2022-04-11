622be937a966bc13f2f7e3acac00436081dd98f9
Lautaro Middi <lautaro.middi@ithk.com.ar>
2022-04-11 19:46:57 -0300

    calculos

--- js.js
+++ js.js
@@ -1,13 +1,90 @@
-var chotaxd = "buenas que tal soy una alerta re molesta jaja";
 
-alert(`${chotaxd}`);
 
-function producto(productos){
-    document.getElementById(producto)
-    if(producto=){
 
+// para el json
+const productosEnVenta = [{
+    name: 'sanguchito',
+    precio: 200,
+    vence: '20/10/2022',
+},
+{
+    name: 'pan',
+    precio: 80,
+    vence: '20/10/2022'
+},
+{
+    name: 'magdalena',
+    precio: 200,
+    vence: '20/10/2022'
+},
+];
+
+function comprar(item){
+    var precioParrafo = document.getElementById("precioParrafo");
+    const 
+
+    if(item = "sanguchito"){
+        precio.innerText = `el precio es ${precio}`
     }
-    else if {
 
-    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// esta en carrito?
+
+// function comprar(producto) {
+//     const buy = document.getElementById(productosEnVenta.name);
+//     var item  = productosEnVenta.find(function(){
+//         return productosEnVenta.nombre
+//     })
+//     alert(`buy es ${buy}`)
+
+//     switch (true) {
+//         case magdalena:
+//             alert(`tu magdalena vale $${productosEnVenta.precio}`);
+//         break;
+//         default: alert('nop');
+
+// }
+
+// const precio = document.getElementById(precio);
+
+// precio.innerText = `el precio es de ${productosEnVenta.precio}`;
+// alert();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
